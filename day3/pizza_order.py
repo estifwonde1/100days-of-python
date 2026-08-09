c7b848c23403cd5abf91b32089f7062acc8bae59
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-09 14:35:33 +0300

    fixed some redundancy and logical errors

--- day3/pizza_order.py
+++ day3/pizza_order.py
@@ -1,27 +1,21 @@
 print("Welcome to PythonPizza")
 order = input("what kinda pizza do u want? 'S' , 'M' , 'L'")
 bill = 0
-add_on = ''
-cheese = ' '
 if order == 'S':
     bill = 15
     print("The bill for Small is $15")
-    add_on = input("would you like Pepperoni? Y or N")
-    cheese = input(" would you like extra cheese? Y or N")
+ 
 elif order == 'M':
     bill = 20
     print("The bill for Medium is $20")
-    add_on = input(" Would you like pepporoni ? Y or N")
-    cheese = input(" would you like extra cheese? Y or N")
-    if add_on == 'Y':
-        bill += 3
 elif order == 'L':
     bill = 25
-    print("The bill for Large is $25")
-    cheese = input(" would you like extra cheese? Y or N")
-       
+    print("The bill for Large is $25")        
 else:
     print("we don't serve that we only serve pizza s , m and l")
+    exit()
+add_on = input("would you like Pepperoni? Y or N")
+cheese = input(" would you like extra cheese? Y or N")
 if add_on == 'Y':
     if order == 'S':
         bill += 2  
