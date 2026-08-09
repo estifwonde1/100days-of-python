41f0bfbdf52adb65692f1d9b165d9a0932137edf
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-09 13:07:31 +0300

    leap year calculator

---
+++ day3/leap_year.py
@@ -0,0 +1,18 @@
+print("welcome to the leap year calculator")
+leap = int(input("please enter the year? "))
+four = int(leap % 4)
+hun = int(leap % 100)
+f_hun=int(leap % 400)
+print (four)
+print(hun)
+print(f_hun)
+if four == 0:
+    if hun == 0:
+        if f_hun == 0:
+            print("The year "+ str(leap) + " is a leap year")
+        else:
+            print("not a leap year")
+    else:
+        print("not a leap year")
+else:
+    print ("not a leap year")
