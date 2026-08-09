ca57a578e3b3d2b836f9992a29fa8d7351bdc1df
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-09 12:09:34 +0300

    build a bmi calculator

--- day3/conditional.py
+++ day3/conditional.py
@@ -1,6 +1,15 @@
-num = int(input("enter a number"))
-even = num % 2
-if even == 0:
-    print("the number is even")
+print("welcome to the BMI calculator")
+height = float(input("please enter ur height(cm)"))
+weight = float(input("please enter ur weight"))
+n = height * height
+bmi = float(weight / n)
+if bmi < 18.5:
+    print("u are under weight eat more")
+elif bmi > 18.5 and bmi < 25:
+    print("u are normal height , good job")
+elif bmi > 25 and bmi < 30:
+    print (" u are slightly over weight , better start to exercise")
+elif bmi > 30 and bmi < 35:
+    print(" u are obese might suggest to start work out seriously")
 else:
-    print("the  number is odd")
+    print ("u are clinically obese u need help")
