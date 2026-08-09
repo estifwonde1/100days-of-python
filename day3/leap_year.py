c087992a9160126fddd59e490c7ce744afdfa1df
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-09 13:22:54 +0300

    refactored for memory usage to follow the rule right less lines

--- day3/leap_year.py
+++ day3/leap_year.py
@@ -1,11 +1,8 @@
 print("welcome to the leap year calculator")
 leap = int(input("please enter the year? "))
-four = int(leap % 4)
-hun = int(leap % 100)
-f_hun=int(leap % 400)
-if four == 0:
-    if hun == 0:
-        if f_hun == 0:
+if leap % 4 == 0:
+    if leap % 100 == 0:
+        if leap % 400 == 0:
             print("The year "+ str(leap) + " is a leap year")
         else:
             print("not a leap year")
