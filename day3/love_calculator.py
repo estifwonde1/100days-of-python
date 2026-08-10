6fdfb84680203b41873d6346b1fcc40fef5e5418
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-10 09:36:54 +0300

    fixed the main function it works

--- day3/love_calculator.py
+++ day3/love_calculator.py
@@ -2,24 +2,43 @@ print("Welcome to the love calculaltor where u can find love or not")
 name = input("please enter ur name").lower()
 another = input("please enter ur crush name").lower()
 n =0
+m = 0
 
-if "t" in name or another:
+if "t" in name:
     n += 1
-elif "r" in name or another:
+if "r" in name:
     n +=1
-elif "u" in name or another :
+if "u" in name:
      n +=1
-elif "e" in name or another:
+if "e" in name:
     n +=1
-elif "l" in name or another :
+if "l" in name:
     n += 1
-elif "o"in name or another:
+if "o"in name:
     n +=1 
-elif "v" in name or another:
+if "v" in name:
     n +=1
-elif "e" in name or another:
+if "e" in name:
     n+=1
-print (f"ur love number is {n}")
+
+if "t" in another :
+    m +=1 
+if "r" in another:
+    m += 1
+if "u" in another:
+    m += 1
+if "e" in another:
+    m +=1
+if "l" in another:
+    m +=1
+if "o" in another:
+    m += 1
+if "v" in another:
+    m += 1
+if "e" in another:
+    m += 1
+
+print (f"ur love number is {str(n) + str(m)}")
 
 
 
