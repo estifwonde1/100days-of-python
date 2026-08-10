5c2084e344b6456922ae6a1cc8f747a0051af158
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-10 09:19:46 +0300

    build the function

--- day3/love_calculator.py
+++ day3/love_calculator.py
@@ -1,4 +1,24 @@
 print("Welcome to the love calculaltor where u can find love or not")
-name = input("please enter ur name")
-another = input("please enter ur crush name")
+name = input("please enter ur name").lower()
+another = input("please enter ur crush name").lower()
+n =0
+
+if t in name or another:
+    n += 1
+elif r in name or another:
+    n +=1
+elif u in name or another :
+     n +=1
+elif e in name or another:
+    n +=1
+elif l in name or another :
+    n += 1
+elif o in name or another:
+    n +=1 
+elif v in name or another:
+    n +=1
+elif e in name or another:
+    n+=1
+
+
 
