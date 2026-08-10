a4cfd4b2eade6579ef597bc8ff75ca7cce491926
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-10 09:24:00 +0300

    fixing some logical errors'

--- day3/love_calculator.py
+++ day3/love_calculator.py
@@ -3,22 +3,23 @@ name = input("please enter ur name").lower()
 another = input("please enter ur crush name").lower()
 n =0
 
-if t in name or another:
+if "t" in name or another:
     n += 1
-elif r in name or another:
+elif "r" in name or another:
     n +=1
-elif u in name or another :
+elif "u" in name or another :
      n +=1
-elif e in name or another:
+elif "e" in name or another:
     n +=1
-elif l in name or another :
+elif "l" in name or another :
     n += 1
-elif o in name or another:
+elif "o"in name or another:
     n +=1 
-elif v in name or another:
+elif "v" in name or another:
     n +=1
-elif e in name or another:
+elif "e" in name or another:
     n+=1
+print (f"ur love number is {n}")
 
 
 
