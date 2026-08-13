fdc166b3bcb5390e69a700f8d80018c2aec70495
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-13 14:44:07 +0300

    defining the charachters

---
+++ day4/rock_paper_scissors.py
@@ -0,0 +1,12 @@
+import random
+print("the fantastic game of rock paper and scissor")
+random_num = random.randstr(1,3)
+wordfy = str(random_num)
+if wordfy == "1":
+    print(r'''
+                ____, O
+            /   /M|
+            /|MMMMMMMM
+            {| | // |}
+            -_}| |/ \ |{_apx
+    ''')
