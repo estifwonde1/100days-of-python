3f590325abeccd63dbb4fc67f29336a8940dbed2
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-13 18:23:43 +0300

    making it better'

--- day4/headsortales.py
+++ day4/headsortales.py
@@ -7,6 +7,8 @@ user =input("choose heads or tales and meer ur fate \n").lower()
 comp = random.choice(choices)
 
 if user == comp:
+    print(comp)
     print("congrats u win")
 else:
+    print(comp)
     print("hahahahahahaha u lose sucka")
