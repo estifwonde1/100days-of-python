59d040451fae38842afee1e89dbb0ae0c06edec1
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-17 13:44:09 +0300

    caluclate ur average

--- day5/average_height.py
+++ day5/average_height.py
@@ -1,1 +1,15 @@
 print("welcome to the average height calulator")
+students_height = []
+add = "yes"
+
+while add == "yes":
+    n = int(input("add the the height of ur students"))
+    add = input("wanna add more yes or no").lower()
+    students_height.append(n)
+
+summing = 0
+for student in students_height:
+    summing += int(student)
+    le = len(students_height)
+    av = summing/le
+print (av)
