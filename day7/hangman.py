c93cc511242e06c27a49be9cac356ed00f58c811
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-18 19:20:59 +0300

    in a nutshell this actually works

--- day7/hangman.py
+++ day7/hangman.py
@@ -25,25 +25,29 @@ for i in range(0,size):
     splited[i] = "_"
 print(splited)
 print(word)
-
-
 ha = list("hangman")
-so = len(ha)
-
 guess = input("guess a letter \n")
+trial = 0
 n = 0
-if guess in word:
-    for i in range(0,size):
-        if word[i] == guess:
-            splited[i] = guess
-else:
-    right.append(ha[n])
-    n += 1
-    print(right)
-        
+while trial <= 6:
+    guess = input("guess a letter \n")
+    if guess in word:
+        for i in range(0,size):
+            if word[i] == guess:
+                splited[i] = guess
+                print(splited)                          
+    else:
+        right.append(ha[n])
+        n += 1
+        trial +=1
+        print(right)
+   
+    if "_" not in splited:
+        print ("u won")
+        break
+    if trial == 7:
+        print(" u hanged an innocent man")
+        print(victim)
+            
 
                 
-            
-    
-      
-print(splited)
