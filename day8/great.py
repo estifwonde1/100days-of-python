82fea477e10d4330d8a1fc862e2b6448279e96ee
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-19 12:10:54 +0300

    alright

--- day8/great.py
+++ day8/great.py
@@ -1,4 +1,6 @@
 print("greetings")
 def greet():
-    print "this is a greeting to the luckiest person who stumbeld upon this mysterious world"
-    
+    print ("this is a greeting to the luckiest person who stumbeld upon this mysterious world")
+    print ("so how is life")
+    print("boy does three things to write really is a lot")
+greet()
