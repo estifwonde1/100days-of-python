29c6ba7dd7ae63bea0245f0f0564dcc0feba51ae
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-21 21:52:32 +0300

    user inputs

--- day9/blind_auction.py
+++ day9/blind_auction.py
@@ -1,1 +1,5 @@
-print 
+print("welcome to the blind auction")
+name= input("what is ur name").lower()
+another = input("is thera another person").lower()
+while another == "yes":
+   name=input("what is ur name").lower() 
