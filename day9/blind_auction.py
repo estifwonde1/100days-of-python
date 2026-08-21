1ebfd947b6a7b8cf0ad2eb23ec2cdfd221b0af4a
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-21 21:57:45 +0300

    build the dictionary for the list

--- day9/blind_auction.py
+++ day9/blind_auction.py
@@ -1,5 +1,9 @@
 print("welcome to the blind auction")
 name= input("what is ur name").lower()
+amount = int(input("how much are u bidding"))
 another = input("is thera another person").lower()
+auction_dict = {}
 while another == "yes":
-   name=input("what is ur name").lower() 
+   name=input("what is ur name").lower()
+   amount=int(input("how much are u bidding").lower
+   auction_dict["name"]=amount 
