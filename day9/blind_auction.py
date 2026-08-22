1145dc5ca51d4227fdbd5fb8b72caad4000bb522
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-22 18:08:42 +0300

    refactored it

--- day9/blind_auction.py
+++ day9/blind_auction.py
@@ -13,29 +13,29 @@ print (r'''
                        .-------------.
                       /_______________\
 ''')
-name = input("what is ur name\n").lower()
-bid = int(input("how much are u bidding for this masterpiece\n"))
-another = input("is another person bidding\n").lower()
-bid_list=[]
-bid_list.append({
-        "Name": name,
-        "Bid" : bid,
-})
 
-while another == "yes":
-    name = input("what is their name\n")
-    bid = int(input("how much are they bidding for this masterpiece\n"))
-    bid_list.append({
-        "Name": name,
-        "Bid": bid,
-        })
-    another = input("is another person bidding\n").lower()
-n = bid_list[0]["Bid"]
-m = bid_list[0]["Name"]
-for i in range (0,len(bid_list)):
-    if n <= bid_list[i]["Bid"]:
-        n = bid_list[i]["Bid"]
-        m = bid_list[i]["Name"]
-print(f"and the winner is {m} who bid this crazy amount {n}")
-    
+
+
+def auction():
+    bid_list=[]
+    bidding_open = True
+    while bidding_open:
+        name = input("what is ur name\n")
+        bid = int(input("how much are they bidding for this masterpiece\n"))
+        bid_list.append({
+            "Name": name,
+            "Bid": bid,
+            })
+        another = input("is another person bidding\n").lower()
+        if another != "yes":
+            bidding_open = False
+    n = bid_list[0]["Bid"]
+    m = bid_list[0]["Name"]
+    for i in range (0,len(bid_list)):
+        if n <= bid_list[i]["Bid"]:
+            n = bid_list[i]["Bid"]
+            m = bid_list[i]["Name"]
+    print(f"and the winner is {m} who bid this crazy amount {n}")
+auction ()
+
 
