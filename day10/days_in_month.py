a333e22b848edc484950ee25bfb94e3dd48956d4
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-22 19:31:19 +0300

    fixed minor error

--- day10/days_in_month.py
+++ day10/days_in_month.py
@@ -16,14 +16,14 @@ def is_leap(year):
         return "not_leap_year"
 def days_in_month(year , month):
     month_days = [31,28,31,30,31,30,31,31,30,31,30,31]
-    indie = int(month -1)   
+    indie = month - 1   
     if is_leap(year) == "leap_year":
         month_days[1] = 29
         return month_days[indie]
     return month_days[indie]
 year = int(input("please enter the year"))
-month = input("enter the month as a number")
+month = int(input("enter the month as a number"))
 
 
-days_in_month(year,month) 
+print(days_in_month(year,month)) 
 
