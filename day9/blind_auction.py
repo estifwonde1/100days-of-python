a57b4430cc8c2de66fdf8f7049fe402478b5cfbf
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-22 17:24:16 +0300

    another day another project

--- day9/blind_auction.py
+++ day9/blind_auction.py
@@ -1,9 +1,11 @@
-print("welcome to the blind auction")
-name= input("what is ur name").lower()
-amount = int(input("how much are u bidding"))
-another = input("is thera another person").lower()
-auction_dict = {}
+print ("welcome to the blind auction center")
+name = input("what is ur name").lower()
+bid = int(input("how much are u bidding for this masterpiece"))
+another = input("is another person bidding").lower()
+bid_list=[]
+
 while another == "yes":
-   name=input("what is ur name").lower()
-   amount=int(input("how much are u bidding").lower
-   auction_dict["name"]=amount 
+    name = input("what is their name")
+    bid = int(input("how much are u bidding for this masterpiece"))
+    bid_list.append({name:bid})
+
