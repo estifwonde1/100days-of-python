6fd76444ccb6ae9b9559ee5a21183db13356ed57
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-22 17:49:52 +0300

    adding the finish touch

--- day9/blind_auction.py
+++ day9/blind_auction.py
@@ -1,4 +1,18 @@
 print ("welcome to the blind auction center")
+print (r'''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\
+                         `'-------'`
+                       .-------------.
+                      /_______________\
+''')
 name = input("what is ur name\n").lower()
 bid = int(input("how much are u bidding for this masterpiece\n"))
 another = input("is another person bidding\n").lower()
