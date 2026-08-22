cfd359f2b98a52fba15ca55fa7646486c2b84952
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-22 17:46:32 +0300

    i built a blind auction logic and will give the winner

--- day9/blind_auction.py
+++ day9/blind_auction.py
@@ -3,12 +3,28 @@ name = input("what is ur name\n").lower()
 bid = int(input("how much are u bidding for this masterpiece\n"))
 another = input("is another person bidding\n").lower()
 bid_list=[]
-bid_list.append({name:bid})
+bid_list.append({
+        "Name": name,
+        "Bid" : bid,
+})
 
 while another == "yes":
-    name = input("what is their name")
-    bid = int(input("how much are u bidding for this masterpiece"))
-    bid_list.append({name:bid})
+    name = input("what is their name\n")
+    bid = int(input("how much are they bidding for this masterpiece\n"))
+    bid_list.append({
+        "Name": name,
+        "Bid": bid,
+        })
+    another = input("is another person bidding\n").lower()
+n = bid_list[0]["Bid"]
+for i in range (0,len(bid_list)):
+    if n <= bid_list[i]["Bid"]:
+        n = bid_list[i]["Bid"]
+print(f"and the winner is the person who bid this amount{n}")
+    
+    
+
+
 
 print(bid_list)
 
