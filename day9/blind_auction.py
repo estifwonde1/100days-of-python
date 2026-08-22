827e9dc689f94558da30c997e78f3568983cfea9
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-22 17:28:29 +0300

    the capturing is done

--- day9/blind_auction.py
+++ day9/blind_auction.py
@@ -1,11 +1,14 @@
 print ("welcome to the blind auction center")
-name = input("what is ur name").lower()
-bid = int(input("how much are u bidding for this masterpiece"))
-another = input("is another person bidding").lower()
+name = input("what is ur name\n").lower()
+bid = int(input("how much are u bidding for this masterpiece\n"))
+another = input("is another person bidding\n").lower()
 bid_list=[]
+bid_list.append({name:bid})
 
 while another == "yes":
     name = input("what is their name")
     bid = int(input("how much are u bidding for this masterpiece"))
     bid_list.append({name:bid})
+
+print(bid_list)
 
