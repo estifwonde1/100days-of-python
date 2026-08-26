602371168f511645b2ec33127e8d0a521daa252f
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-26 13:20:02 +0300

    buildind the exception for the random print the same value for higher and lower

--- day15/higher_lower/main.py
+++ day15/higher_lower/main.py
@@ -7,9 +7,17 @@ while not is_gameover:
     num = len(data)
     indie = random.randint(0,num-1)
     boomy = random.randint(0,num-1)
+    try higher == lower:
+        
 
-    higher = data[indie]["follower_count"]
-    lower = data[boomy][ "follower_count"]
-    print(higher)
-    print(lower)
+    higher = data[indie]
+    needed_data = ["name","follower_count"]
+    values = [higher[k] for k in needed_data]
+    print(values[0])
+    lower = data[boomy]
+    lower_needed_data = ["name","follower_count"]
+    lower_value = [lower[k] for k in lower_needed_data]
+    print(lower_value[0])
+
+
     break
