8436e25884bfe01fb33192bb12523014852362d9
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-26 17:17:04 +0300

    put it in function so it's easly reusable

--- day15/higher_lower/main.py
+++ day15/higher_lower/main.py
@@ -5,45 +5,44 @@ print(art.logo)
 def ask_to_play():
     ask = input("want to play again: yes or no \n").lower()
     return ask == "yes"
-        
-
-playing = True
-while playing:
-    score =0
-    again = True
-    while again:
-        
-        num = len(data)
-        indie = random.randint(0,num-1)
-        boomy = random.randint(0,num-1)
-        if indie == boomy:
-            continue
-        higher = data[indie]
-        needed_data = ["name","follower_count"]
-        values = [higher[k] for k in needed_data]
-        print(values[0])
-        print(art.vs)
-        lower = data[boomy]
-        lower_needed_data = ["name","follower_count"]
-        lower_value = [lower[k] for k in lower_needed_data]
-        print(lower_value[0])
-        question = input("higher or lower\n").lower()
-        if question == "higher":
-            if values[1] > lower_value[1]:
-                score += 1
-                print(score)
+def main_game():
+    playing = True
+    while playing:
+        score =0
+        again = True
+        while again:       
+            num = len(data)
+            indie = random.randint(0,num-1)
+            boomy = random.randint(0,num-1)
+            if indie == boomy:
+                continue
+            higher = data[indie]
+            needed_data = ["name","follower_count"]
+            values = [higher[k] for k in needed_data]
+            print(values[0])
+            print(art.vs)
+            lower = data[boomy]
+            lower_needed_data = ["name","follower_count"]
+            lower_value = [lower[k] for k in lower_needed_data]
+            print(lower_value[0])
+            question = input("higher or lower\n").lower()
+            if question == "higher":
+                if values[1] > lower_value[1]:
+                    score += 1
+                    print(score)
+                else:
+                    print(f"u lost with final score {score}")
+                    again = False
+            elif question == "lower":
+                if values[1] < lower_value[1]:
+                    score += 1
+                    print(score) 
+                else:
+                    print(f"{art.final} {score}")
+                    again = False
             else:
-                print(f"u lost with final score {score}")
-                again = False
-        elif question == "lower":
-            if values[1] < lower_value[1]:
-                score += 1
-                print(score) 
-            else:
-                print(f"{art.final} {score}")
-                again = False
-        else:
-            print("invalid input please enter higher or lower")
-            continue
-                
-    playing = ask_to_play()
+                print("invalid input please enter higher or lower")
+                continue
+                    
+        playing = ask_to_play()
+main_game()
