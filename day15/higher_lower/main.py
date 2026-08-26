7d3a684ee94d84be4943505cccddb972710bd26b
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-26 16:22:24 +0300

    the same output for both higher and lower is now fixed

--- day15/higher_lower/main.py
+++ day15/higher_lower/main.py
@@ -7,13 +7,13 @@ while not is_gameover:
     num = len(data)
     indie = random.randint(0,num-1)
     boomy = random.randint(0,num-1)
-    try higher == lower:
-        
-
+    if indie == boomy:
+        continue
     higher = data[indie]
     needed_data = ["name","follower_count"]
     values = [higher[k] for k in needed_data]
     print(values[0])
+    print(art.vs)
     lower = data[boomy]
     lower_needed_data = ["name","follower_count"]
     lower_value = [lower[k] for k in lower_needed_data]
