e490df229188d55c831a1229d4d5cdb1c48c380e
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-27 11:39:21 +0300

    the prototype for all drinks is made

--- day15/cofee_machine.py
+++ day15/cofee_machine.py
@@ -26,7 +26,7 @@ price_tag ={
 }
 
 
-def make_espresso():
+def make_latte():
     global water
     global milk
     global Coffee
@@ -35,9 +35,37 @@ def make_espresso():
     milk -= 50
     Coffee -= 24
     Money += 2.42
-    print ("here is ur espresso")
+    print ("here is ur latte")
     print(f"water:{water}\nmilk:{milk}\ncoffee:{Coffee}\nMoney:{Money}")
-order =input("what would you like? (espresso/latte/cappuciono)\n")
+order =input("what would you like? (espresso/latte/cappuccino)\n")
+def make_espresso():
+    global water
+    global Coffee
+    global Money
+    water -= 100
+    Coffee -= 15
+    Money += 4.50
+    print ("here is ur espresso")
+def make_cappucino():
+    global water
+    global milk
+    global Coffee
+    global Money
+    water -= 36 
+    milk -= 100
+    Coffee -= 18
+    Money += 5.40
+    print ("here is ur cappuccino")
+def refill():
+    global water
+    global milk
+    global Coffee
+    water += 300
+    milk += 100
+    Coffee += 100
+
+
+
 if order == "espresso":
     make_espresso()
 #am in progress am understanding the jist of it
