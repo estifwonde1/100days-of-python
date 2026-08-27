8e04ba380f76dc00478391bd4179a89c3b0ac490
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-27 10:23:37 +0300

    prototype is on making

--- day15/cofee_machine.py
+++ day15/cofee_machine.py
@@ -4,13 +4,13 @@ dP   `" dP   Yb 88__   88__   88__   88__     88b  d88   dPYb   dP   `" 88  88 8
 Yb      Yb   dP 88""   88""   88""   88""     88YbdP88  dP__Yb  Yb      888888 88 88 Y88 88""   
  YboodP  YbodP  88     88     888888 888888   88 YY 88 dP""""Yb  YboodP 88  88 88 88  Y8 888888 
 ''')
-order =input("what would you like? (espresso/latte/cappuciono)\n")
 
-def store():
-    water = 300
-    milk = 200
-    Coffee = 100
-    print(f"water:{water}\nmilk:{milk}\ncoffee:{Coffee}")
+
+water = 300
+milk = 200
+Coffee = 100
+Money = 0
+print(f"water:{water}\nmilk:{milk}\ncoffee:{Coffee}\nMoney:{Money}")
 
  
 currency_value = {
@@ -18,27 +18,30 @@ currency_value = {
     "dime" : 0.10,
     "nickel": 0.5,
     "penny":0.01,
+}          
+price_tag ={
+    "espresso" : 2.42,
+    "latte" : 4.50,
+    "cappuccino" : 5.40
 }
-  
-    
-    
+
 
 def make_espresso():
-    print("the price is $2.42\n")
-    print("please insert coints:\n")
-    bill = 2.42
-    tar = 0
-    quarter = float(input("please insert quarters: "))
-    dime = float(input("please insert dime:  "))
-    nickel = float(input("please insert quarter: "))
-    penny = float(input("please insert penny: "))
-    change = float(quarter + dime + nickel + penny)
-    for q in range (quarter):
-        tar += currency_value["quarter"]
+    global water
+    global milk
+    global Coffee
+    global Money
+    water -= 200
+    milk -= 50
+    Coffee -= 24
+    Money += 2.42
+    print ("here is ur espresso")
+    print(f"water:{water}\nmilk:{milk}\ncoffee:{Coffee}\nMoney:{Money}")
+order =input("what would you like? (espresso/latte/cappuciono)\n")
+if order == "espresso":
+    make_espresso()
 
-    print(f"here is ur expresso and ur change {change} \n have a great day")
 
-#done for today am stopping at solving the conversion i will continue tommorrow 
         
 
     
