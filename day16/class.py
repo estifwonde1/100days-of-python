d26e6a36ec16f1b79a747b88d0c168968410051a
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-29 08:49:26 +0300

    timmy is a fast turtle

--- day16/class.py
+++ day16/class.py
@@ -1,7 +1,12 @@
 from turtle import Turtle,Screen
 timmy = Turtle()
 
+
+
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+print(timmy)
 my_screen = Screen()
 print(my_screen.canvheight)
 my_screen.exitonclick()
-print(timmy)
