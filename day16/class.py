00f622fb5f1e5ae754439e0e5f9a5bf01be8e057
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-29 18:21:33 +0300

    understanding packages

--- day16/class.py
+++ day16/class.py
@@ -11,5 +11,9 @@
 # my_screen = Screen()
 # print(my_screen.canvheight)
 # my_screen.exitonclick()
-from preetytable import PrettyTable
+from prettytable import PrettyTable
 table = PrettyTable()
+table.add_column("pokemen_name",["pokemon 1","pokemon 2","pokemon3"])
+table.add_column("type",["electric","fire","water"])
+print(table)
+
