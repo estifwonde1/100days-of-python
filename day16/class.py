04567ad5e5283255098fc926f0032de5777067a5
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-29 18:56:07 +0300

    still learning

--- day16/class.py
+++ day16/class.py
@@ -15,5 +15,6 @@ from prettytable import PrettyTable
 table = PrettyTable()
 table.add_column("pokemen_name",["pokemon 1","pokemon 2","pokemon3"])
 table.add_column("type",["electric","fire","water"])
+table.align = "l"
 print(table)
 
---
+++ day17/main.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self,user_id,username):
+        self.id = user_id
+        self.username = username
+user = User()
+user.id = " 001"
+user.username = "estif"
