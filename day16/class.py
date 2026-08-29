70d9d257143ca0c5721ceae520917cf3c4bed30e
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-29 08:42:26 +0300

    using turtle

--- day16/class.py
+++ day16/class.py
@@ -1,3 +1,7 @@
-import turtle
-timmy = turtle.Turtle()
+from turtle import Turtle,Screen
+timmy = Turtle()
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
 print(timmy)
