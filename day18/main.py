5be5bd70e973858f5213ea12aa1e2e547c93fff4
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-31 15:14:41 +0300

    first challenge completed

---
+++ day18/main.py
@@ -0,0 +1,19 @@
+from turtle import Turtle,Screen
+
+mini = Turtle()
+mini.shape("turtle")
+mini.color("blue")
+n = 5
+for i in range (0,n):
+    mini.right(90)
+    mini.forward(100)
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
