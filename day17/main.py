6f11fc7d21623058ed67ec16ee6358f178607eeb
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-08-31 11:53:02 +0300

    the basic class of the function works

--- day17/main.py
+++ day17/main.py
@@ -5,12 +5,24 @@ class User:
         self.id = user_id
         self.score = 0
     def ask_question(self,score):
-        num = len(data_quiz_questions)
-        indie = random.randint(0,num-1)
-        que = data_quiz_questions[indie]
-        needed_data = ["question","answer"]
-        values = [que[k] for k in needed_data]
-        print(values[0])
-        ask = input("True or False\n")
-        if ask == values[1]:
-            self.score += 1
+        play = True
+        while play:
+            num = len(data_quiz_questions)
+            indie = random.randint(0,num-1)
+            que = data_quiz_questions[indie]
+            needed_data = ["question","answer"]
+            values = [que[k] for k in needed_data]
+            print(values[0])
+            print(values[1])
+            ask = input("True or False\n")
+            answer = values[1]
+            if ask == answer:
+                self.score += 1
+                print(score)
+            elif ask != values[1]:
+                print(f"wrong answer {score}")
+                play = False
+            else:
+                print("invalidinput")
+user1 = User("001")
+user1.ask_question(0)
