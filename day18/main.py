f12c6a173b3e0c5fb7d66c222dc07f0b6ff46891
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-01 21:28:38 +0300

    skip line donw

--- day18/main.py
+++ day18/main.py
@@ -7,12 +7,18 @@ lili = Turtle()
 lili.shape("turtle")
 lili.color("green")
 n = 5
-for i in range (0,n):
-    mini.right(90)
-    mini.forward(100)
-for _ in range (10):
+# for i in range (0,n):
+#     mini.right(90)
+#     mini.forward(100)
+for _ in range (50):
+    
+    lili.pendown()
+    lili.forward(15)
+    lili.penup()
     lili.forward(10)
-    lili.right(10)
+    lili.pencolor("brown")
+  
+
 
 
 
