fc851ae9fc4a85334207eb70e266646ff4153eb8
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-01 21:36:56 +0300

    shapes

---
+++ day18/10_shapes.py
@@ -0,0 +1,48 @@
+import random
+from turtle import Turtle , Screen
+
+mini = Turtle()
+#let's take break for today my mind is all over the place but tommorrow we will resume our focused journey
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen
+screen.exitonclick()
