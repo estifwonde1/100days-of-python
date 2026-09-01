7999b35c322328b903c0182174c15539049f584b
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-01 09:30:18 +0300

    gui

--- day18/main.py
+++ day18/main.py
@@ -3,10 +3,16 @@ from turtle import Turtle,Screen
 mini = Turtle()
 mini.shape("turtle")
 mini.color("blue")
+lili = Turtle()
+lili.shape("turtle")
+lili.color("green")
 n = 5
 for i in range (0,n):
     mini.right(90)
     mini.forward(100)
+for _ in range (10):
+    lili.forward(10)
+    lili.right(10)
 
 
 
