bb53663b2686bbd47c9634ba8155c878c8c0e527
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-02 19:42:09 +0300

    3 out of 10 shapes done

--- day18/10_shapes.py
+++ day18/10_shapes.py
@@ -3,16 +3,27 @@ from turtle import Turtle , Screen
 
 mini = Turtle()
 mini.shape()
-
-for _ in range (4):   
-    mini.right(90)
+for _ in range (3):
     mini.forward(100)
+    mini.right(120)
+mini.forward(100)
+for _ in range (4):   
+   mini.right(90)
+   mini.forward(100)
 mini.backward(50)
-for _ in range (6):
+for _ in range (5):
     mini.forward(50)
     mini.right(60)
     mini.forward(50)
 mini.forward(50)
+mini.right(60)
+mini.forward(100)
+for _ in range (5):
+    mini.right(55)
+    mini.forward(100)
+#    mini.right(50)
+mini.forward(50)
+
 
 
 
