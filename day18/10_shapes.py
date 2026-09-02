e733d259acf5ef5a1ee41ec511828f51561c37f3
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-02 20:49:58 +0300

    4 out of 10 complete

--- day18/10_shapes.py
+++ day18/10_shapes.py
@@ -1,69 +1,26 @@
 import random
-from turtle import Turtle , Screen
+from turtle import Turtle, Screen
 
 mini = Turtle()
 mini.shape()
 for _ in range (3):
-    mini.forward(100)
     mini.right(120)
-mini.forward(100)
-for _ in range (4):   
-   mini.right(90)
-   mini.forward(100)
-mini.backward(50)
-for _ in range (5):
-    mini.forward(50)
-    mini.right(60)
-    mini.forward(50)
-mini.forward(50)
-mini.right(60)
-mini.forward(100)
-for _ in range (5):
-    mini.right(55)
     mini.forward(100)
-#    mini.right(50)
-mini.forward(50)
 
 
+for _ in range (4):
+    mini.right(90)
+    mini.forward(100)
+mini.backward(100)
+for _ in range (5):
+    mini.forward(100)
+    mini.right(72)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+mini.forward(100)
+for _ in range(6):
+    mini.right(60)
+    mini.forward(100)
+mini.right(100)
 
 
 screen = Screen()
