c292916fe6626b061eaa74db8b87fe4afd7500da
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-03 22:33:57 +0300

    refactoring phase 1 complete

--- day18/10_shapes.py
+++ day18/10_shapes.py
@@ -7,68 +7,48 @@ colors = ["crimson","darkorange","gold","forestgreen","darkcyan","deepskyblue","
 num = len(colors)
 
 degrees = [120,90,72,60,51.43,45,40,36,32.73,30]
-ro = True
-
-#will continue from here tommorrow
-pick = random.randint(0,num-1)
-randomize = colors[pick]
-mini.color(randomize)
+def randoming():
+    pick = random.randint(0,num-1)
+    randomize = colors[pick]
+    mini.color(randomize)
 for _ in range (3):
     mini.right(120)
     mini.forward(100)
-pick = random.randint(0,num-1)
-randomize = colors[pick]
-mini.color(randomize)
+randoming()
 for _ in range (4):
     mini.right(90)
     mini.forward(100)
 mini.backward(100)
-pick = random.randint(0,num-1)
-randomize = colors[pick]
-mini.color(randomize)
+randoming()
 for _ in range (5):
     mini.forward(100)
     mini.right(72)
 mini.forward(100)
-pick = random.randint(0,num-1)
-randomize = colors[pick]
-mini.color(randomize)
+randoming()
 for _ in range(6):
     mini.right(60)
     mini.forward(100)
-pick = random.randint(0,num-1)
-randomize = colors[pick]
-mini.color(randomize)
+randoming()
 for _ in range(7):
     mini.right(51.43)
     mini.forward(100)
-pick = random.randint(0,num-1)
-randomize = colors[pick]
-mini.color(randomize)
+randoming()
 for _ in range(8):
     mini.right(45)
     mini.forward(100)
-pick = random.randint(0,num-1)
-randomize = colors[pick]
-mini.color(randomize)
+randoming()
 for _ in range(9):
     mini.right(40)
     mini.forward(100)
-pick = random.randint(0,num-1)
-randomize = colors[pick]
-mini.color(randomize)
+randoming()
 for _ in range (10):
     mini.right(36)
     mini.forward(100)
-pick = random.randint(0,num-1)
-randomize = colors[pick]
-mini.color(randomize)
+randoming()
 for _ in range (11):
     mini.right(32.73)
     mini.forward(100)
-pick = random.randint(0,num-1)
-randomize = colors[pick]
-mini.color(randomize)
+randoming()
 for _ in range (12):
     mini.right(30)
     mini.forward(100)
