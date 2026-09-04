0803f5a3ac7aaa0732b2f4bf24f1d179c1384ddf
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-04 17:20:28 +0300

    refactored

--- day18/random_walk.py
+++ day18/random_walk.py
@@ -5,16 +5,14 @@ from shapes import colors
 
 mini = Turtle()
 
-walks = [0,90,180,270,360]
-num_walks = 500
-for _ in range(num_walks):
+walks = [0,90,180,270]
+
+for _ in range(200):
     pick = random.randint(0,len(colors)-1)
     randomize = colors[pick]   
-    swag = random.randint(0,len(walks)-1)
-    forr = walks[swag]
     mini.pensize(7)
     mini.speed("fast")
-    mini.right(forr)
+    mini.setheading(random.choice(walks))
     mini.forward(30)
     mini.color(randomize)
     
