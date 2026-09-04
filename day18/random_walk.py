8420256242a61cb5e6392e5baaf2318446333969
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-04 17:13:04 +0300

    random walk completed

--- day18/random_walk.py
+++ day18/random_walk.py
@@ -1,12 +1,25 @@
 from turtle import Screen, Turtle
-from shapes import randoming
+from shapes import randoming,colors
 import random
 mini = Turtle()
-randoming()
 
-num_walks = 10
+walks = [0,90,180,270,360]
+num_walks = 500
 for _ in range(num_walks):
-    mini.forward(20)
+    pick = random.randint(0,len(colors)-1)
+    randomize = colors[pick]   
+    swag = random.randint(0,len(walks)-1)
+    forr = walks[swag]
+    mini.pensize(7)
+    mini.speed("fast")
+    mini.right(forr)
+    mini.forward(30)
+    mini.color(randomize)
+    
+   
+    
+ 
+
 
 
 
