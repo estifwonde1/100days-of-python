336a4a8966b623d20fd938dd308fe268508d5514
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-04 18:57:06 +0300

    after some serious confusion sphyrogram is complete

---
+++ day18/spirograph.py
@@ -0,0 +1,46 @@
+import random
+import turtle as t
+
+mini = t.Turtle()
+t.colormode(255)
+def colors():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    return(r,g,b)
+
+def circles():
+    n = 360
+    m = 0
+    while m <= n:
+        
+        # heads = 360 / n
+        mini.speed("fastest")
+        mini.circle(100)
+        mini.setheading(m)
+        mini.color(colors())   
+        m += 1
+        
+
+
+circles()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = t.Screen()
+screen.exitonclick()
