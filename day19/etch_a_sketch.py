6903256f9cd4c8fb221e54d2268c2ae15facd749
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-08 13:48:46 +0300

    main functionalities for the direction works

--- day19/etch_a_sketch.py
+++ day19/etch_a_sketch.py
@@ -2,9 +2,21 @@ import turtle as t
 
 mini = t.Turtle()
 
-def move():
+def move_forward():
     mini.forward(50)
+def move_backward():
+    mini.backward(50)
+def move_left():
+    mini.left(90)
+def move_right():
+    mini.right(90)
+
+
+
 screen = t.Screen()
 screen.listen()
-screen.onkey(key ="space",fun=move)
+screen.onkey(key ="w",fun=move_forward)
+screen.onkey(key="s",fun=move_backward)
+screen.onkey(key="a",fun=move_left)
+screen.onkey(key="d",fun=move_right)
 screen.exitonclick()
