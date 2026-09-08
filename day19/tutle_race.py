f6cd66b46d0097bc5e1e0cfec7a93dc882397639
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-08 16:04:42 +0300

    refactoring it

--- day19/tutle_race.py
+++ day19/tutle_race.py
@@ -4,49 +4,21 @@ screen = t.Screen()
 screen.setup(width=500,height=400)
 guess = screen.textinput(title ="welcome",prompt="choose ur color")
 print(guess)
+color = ["red","yellow","green","purple","blue","orange"]
+y_postions = [-70,-40,-10,20,50,80]
 
 def race(color): 
     distance = random.randint(0,10)
     color.forward(distance)
 
 n = -230
-green = t.Turtle()
-green.shape("turtle")
-green.color("green")
-green.penup()
-green.goto(n,-30)
-purple= t.Turtle()
-purple.shape("turtle")
-purple.color("purple")
-purple.penup()
-purple.goto(n,-60)
-blue = t.Turtle()
-blue.shape("turtle")
-blue.color("blue")
-blue.penup()
-blue.goto(n,-90)
-yellow =t.Turtle()
-yellow.shape("turtle")
-yellow.color("yellow")
-yellow.penup()
-yellow.goto(n,0)
-orange = t.Turtle()
-orange.shape("turtle")
-orange.color("orange")
-orange.penup()
-orange.goto(n,30)
-red = t.Turtle()
-red.shape("turtle")
-red.color("red")
-red.penup()
-red.goto(n,60)
-for _ in range (90):
-    race(green)
-    race(purple)
-    race(blue)
-    race(yellow)
-    race(orange)
-    race(red)
+for _ in range(6):
+    mini = t.Turtle(shape="turtle")
+    mini.penup()
+    mini.goto(n,y_postions[_])
+    mini.color(color[_])
+
+
 
 
 
