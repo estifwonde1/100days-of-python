d2ebcc4ffb4743deb8387999df16c7a814e9841c
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-08 15:02:00 +0300

    all turtles are created as an instance

---
+++ day19/tutle_race.py
@@ -0,0 +1,44 @@
+import random
+import turtle as t
+n = -930
+green = t.Turtle()
+green.shape("turtle")
+green.color("green")
+green.penup()
+green.goto(n,-100)
+purple= t.Turtle()
+purple.shape("turtle")
+purple.color("purple")
+purple.penup()
+purple.goto(n,-200)
+blue = t.Turtle()
+blue.shape("turtle")
+blue.color("blue")
+blue.penup()
+blue.goto(n,-300)
+yellow =t.Turtle()
+yellow.shape("turtle")
+yellow.color("yellow")
+yellow.penup()
+yellow.goto(n,0)
+orange = t.Turtle()
+orange.shape("turtle")
+orange.color("orange")
+orange.penup()
+orange.goto(n,100)
+red = t.Turtle()
+red.shape("turtle")
+red.color("red")
+red.penup()
+red.goto(n,200)
+
+
+
+
+
+
+
+
+
+screen = t.Screen()
+screen.exitonclick()
