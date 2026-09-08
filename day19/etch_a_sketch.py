d759c91c6b083b59307ff19c81483c13c29edfc4
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-08 11:54:16 +0300

    learning event listenrs

--- day19/etch_a_sketch.py
+++ day19/etch_a_sketch.py
@@ -2,29 +2,9 @@ import turtle as t
 
 mini = t.Turtle()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def move():
+    mini.forward(50)
 screen = t.Screen()
-screen.exitonclick()
+screen.listen()
+screen.onkey(key = "space" ,fun = move())
+# screen.exitonclick()
