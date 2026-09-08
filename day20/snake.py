55c7311f0413b7beed9c52dcbd1ea181178572aa
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-08 17:04:28 +0300

    building snake

---
+++ day20/snake.py
@@ -0,0 +1,31 @@
+import random
+import turtle as t
+
+
+screen = t.Screen()
+
+y = [10,20,30]
+for _ in range (3):
+    snakey = t.Turtle(shape = "square")
+    snakey.goto(y[_],0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
