1c6769e6c01c1b2cac5cfbbd33a5e9fd835c453f
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-08 16:41:42 +0300

    refactored and works

--- day19/tutle_race.py
+++ day19/tutle_race.py
@@ -1,22 +1,38 @@
 import random
 import turtle as t
+
 screen = t.Screen()
 screen.setup(width=500,height=400)
+is_race_on = False
 guess = screen.textinput(title ="welcome",prompt="choose ur color")
 print(guess)
 color = ["red","yellow","green","purple","blue","orange"]
 y_postions = [-70,-40,-10,20,50,80]
+atheletes = []
 
-def race(color): 
-    distance = random.randint(0,10)
-    color.forward(distance)
-
-n = -230
 for _ in range(6):
     mini = t.Turtle(shape="turtle")
     mini.penup()
-    mini.goto(n,y_postions[_])
+    mini.goto(-230,y_postions[_])
     mini.color(color[_])
+    atheletes.append(mini)
+
+if guess:
+    is_race_on = True
+    
+while is_race_on:
+    for turtle in atheletes:
+        if turtle.xcor() > 230:
+            print(turtle.pencolor())
+            is_race_on = False
+            if guess == turtle.pencolor():
+                print("congrats u won")
+            else:
+                print("womp womp u lost ")
+                turtle.clear()     
+        distance = random.randint(0,10)
+        turtle.forward(distance)
+
 
 
 
