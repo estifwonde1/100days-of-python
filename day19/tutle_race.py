a823cd48fed50bd4c173e64adbf99f52fa4c6717
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-08 15:20:08 +0300

    setting up the screen and stuff

--- day19/tutle_race.py
+++ day19/tutle_race.py
@@ -1,21 +1,25 @@
 import random
 import turtle as t
-n = -930
+screen = t.Screen()
+screen.setup(width=500,height=400)
+guess = screen.textinput(title ="welcome",prompt="choose ur color")
+print(guess)
+n = -230
 green = t.Turtle()
 green.shape("turtle")
 green.color("green")
 green.penup()
-green.goto(n,-100)
+green.goto(n,-30)
 purple= t.Turtle()
 purple.shape("turtle")
 purple.color("purple")
 purple.penup()
-purple.goto(n,-200)
+purple.goto(n,-60)
 blue = t.Turtle()
 blue.shape("turtle")
 blue.color("blue")
 blue.penup()
-blue.goto(n,-300)
+blue.goto(n,-90)
 yellow =t.Turtle()
 yellow.shape("turtle")
 yellow.color("yellow")
@@ -25,12 +29,12 @@ orange = t.Turtle()
 orange.shape("turtle")
 orange.color("orange")
 orange.penup()
-orange.goto(n,100)
+orange.goto(n,30)
 red = t.Turtle()
 red.shape("turtle")
 red.color("red")
 red.penup()
-red.goto(n,200)
+red.goto(n,60)
 
 
 
@@ -40,5 +44,6 @@ red.goto(n,200)
 
 
 
-screen = t.Screen()
+
+
 screen.exitonclick()
