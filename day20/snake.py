d320a9bd2ebd5ba76305a526f37cd081c8dda473
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-09 19:33:17 +0300

    the food eating and then randomly appearing works

--- day20/snake.py
+++ day20/snake.py
@@ -6,13 +6,7 @@ screen = t.Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("snakey cake")
-food = t.Turtle("circle")
-food.color("green")
-food.shapesize(0.5,0.5)
-food.penup()
-x_pos = random.randint(-300,300)
-y_pos = random.randint(-300,300)
-food.goto(x_pos,y_pos)
+
 segement_postions = [(0,0),(-20,0),(-40,0)]
 fully = []
 for _ in segement_postions:
@@ -22,11 +16,16 @@ for _ in segement_postions:
     snakey.goto(_)
     fully.append(snakey)
 is_gameover = True
+#food section myboy don't forget
+food = t.Turtle("circle")
+food.color("green")
+food.shapesize(0.5,0.5)
+food.penup()
 x_pos = random.randint(-300,300)
 y_pos = random.randint(-300,300)
-
-while is_gameover: 
-    food.goto(x_pos,y_pos)
+food.speed("fastest")
+food.setpos(x_pos,y_pos)
+while is_gameover:   
     snakey.penup()
     snakey.forward(10)
     snakey.speed(1)
@@ -34,14 +33,18 @@ while is_gameover:
         if snake.xcor() > 280 or snake.xcor() < -280 or snake.ycor() > 280 or snake.ycor() < -280:
             is_gameover = False
         def move_right():
-            new_heading = snake.heading() + 90
-            snake.setheading(new_heading)
+            new_heading = snakey.heading() + 90
+            snakey.setheading(new_heading)
         def move_left():
-            new_heading = snake.heading() - 90
-            snake.setheading(new_heading)
-        if snake.xcor() == food.position():
+            new_heading = snakey.heading() - 90
+            snakey.setheading(new_heading)
+        if snake.distance(food)<15:
+            x_pos = random.randint(-300,300)
+            y_pos = random.randint(-300,300)
             food.clear()
             food.goto(x_pos,y_pos)
+            print(snake.position(),food.position())
+
 
 
     screen.listen()
