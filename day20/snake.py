daaba1efda04245db5ba40321d0a1cc1c3823dab
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-09 19:06:37 +0300

    the movemment of the snake and control is done

--- day20/snake.py
+++ day20/snake.py
@@ -1,25 +1,43 @@
-import random
 import turtle as t
+import random
 
 
 screen = t.Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
-screen.title("snakey cake ")
+screen.title("snakey cake")
+food = t.Turtle("circle")
+food.color("green")
+food.shapesize(0.5,0.5)
+food.penup()
+food.goto(40,40)
 segement_postions = [(0,0),(-20,0),(-40,0)]
-
-size = []
+fully = []
 for _ in segement_postions:
     snakey = t.Turtle(shape = "square")
     snakey.color("white")
+    snakey.penup()
     snakey.goto(_)
+    fully.append(snakey)
+is_gameover = True
+while is_gameover:
+    snakey.penup()
+    snakey.forward(10)
+    snakey.speed(1)
+    for snake in fully:
+        if snake.xcor() > 280 or snake.xcor() < -280 or snake.ycor() > 280 or snake.ycor() < -280:
+            is_gameover = False
+        def move_right():
+            new_heading = snake.heading() + 90
+            snake.setheading(new_heading)
+        def move_left():
+            new_heading = snake.heading() - 90
+            snake.setheading(new_heading)
+
+    screen.listen()
+    screen.onkey(key ="d",fun = move_right)
+    screen.onkey(key = "a",fun = move_left)
     
-
-
-
-
-
-
 
 
 
