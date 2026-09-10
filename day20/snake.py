12fd7356ea85f498eb6a5b10de38341aecedb1f0
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-10 14:48:36 +0300

    everything works only thing that is left is the addingof tails

--- day20/snake.py
+++ day20/snake.py
@@ -1,35 +1,21 @@
 import turtle as t
 import random
 
-
+#setups
 screen = t.Screen()
-
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("snakey cake")
+screen.tracer(0)
 score_board = t.Turtle()
 score_board.hideturtle()
 score = 0
 score_board.color("white")
-
 score_board.penup()
 score_board.speed("fastest")
 score_board.shapesize(1,1)
 score_board.setpos(0,270)
 score_board.write(score,font =("ariel",20,"bold"))
-    
-
-
-segement_postions = [(0,0),(-20,0),(-40,0)]
-fully = []
-for _ in segement_postions:
-    snakey = t.Turtle(shape = "square")
-    snakey.color("white")
-    snakey.penup()
-    snakey.goto(_)
-    fully.append(snakey)
-is_gameover = True
-#food section myboy don't forget
 food = t.Turtle("circle")
 food.color("green")
 food.shapesize(0.5,0.5)
@@ -38,30 +24,34 @@ x_pos = random.randint(-290,290)
 y_pos = random.randint(-290,290)
 food.speed("fastest")
 food.setpos(x_pos,y_pos)
-while is_gameover:   
+head = t.Turtle(shape="square")
+head.penup()
+head.color("white")
+segement_postions = [(-20,0),(-40,0)]
+fully = []
+for _ in segement_postions:
+    snakey = t.Turtle(shape = "square")
+    snakey.color("white")
     snakey.penup()
-    # snakey.forward(10)
-    snakey.speed(1)
-    for snake in fully:
-        if snake.xcor() > 280 or snake.xcor() < -280 or snake.ycor() > 280 or snake.ycor() < -280:
-            is_gameover = False
-            retry = screen.textinput(title ="Game Over" ,prompt="wanna play again" )
-            if retry == "yes":
-                is_gameover = True
-            else:
-                screen.exitonclick()
-#yeah am really confused on how i can move all 3 blocks getting dictated by the first 1 as head
-#the trick is to use the fronts postion change to be the next ones change and so on and i will try to figure that out tommorrow but for today solid work proud of myself
-        snake.forward(10)
-        def move_right():
-                new_heading = snakey.heading() + 90
-                snakey.setheading(new_heading)
-                snakey.forward(10)
-        def move_left():
-                new_heading = snakey.heading() - 90
-                snakey.setheading(new_heading)
-                snakey.forward(10)
-    if snake.distance(food)<15:
+    snakey.goto(_)
+    fully.append(snakey)
+
+pos_history =[]
+step_delay = 3
+def move():
+    pos_history.insert(0,head.pos())
+    head.forward(10)
+    head.speed(1)
+    for index,snakey in enumerate(fully):
+        history = (index + 1) *3
+        if len(pos_history) > history:
+            snakey.goto(pos_history[history])
+    if len(pos_history) > 10:
+        pos_history.pop()
+    if head.xcor() > 280 or head.xcor() < -280 or head.ycor() > 280 or head.ycor() < -280:
+        screen.exitonclick()
+    if head.distance(food) < 15:
+        global score
         x_pos = random.randint(-290,290)
         y_pos = random.randint(-290,290)
         food.clear()
@@ -69,15 +59,30 @@ while is_gameover:
         score_board.clear()          
         score += 1
         score_board.write(score,font =("ariel",20,"bold"))
-        print(snake.position(),food.position())
+        print(head.position(),food.position())
+    screen.update()
+    screen.ontimer(move,20)
+def move_right():
+    new_heading = head.heading() + 90
+    head.setheading(new_heading)
+def move_left():
+    new_heading = head.heading() - 90
+    head.setheading(new_heading)
+is_gameover = True
+
+
+move() 
+   
+
+#yeah am really confused on how i can move all 3 blocks getting dictated by the first 1 as head
+#the trick is to use the fronts postion change to be the next ones change and so on and i will try to figure that out tommorrow but for today solid work proud of myself   
 
 
 
+screen.listen()
+screen.onkey(key ="a",fun = move_right)
+screen.onkey(key = "d",fun = move_left)
 
-    screen.listen()
-    screen.onkey(key ="a",fun = move_right)
-    screen.onkey(key = "d",fun = move_left)
-    
 
 
 
