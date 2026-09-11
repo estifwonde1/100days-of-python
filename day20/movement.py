af2e8778d12066be5bd851fa164be53273f69f0f
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-11 11:32:23 +0300

    trying to come up with a better solution

--- day20/movement.py
+++ day20/movement.py
@@ -8,7 +8,13 @@ screen.bgcolor("black")
 screen.tracer(0)
 segment_postions =[(0,0),(-20,0),(-40,0)]
 segments = []
-for _ in segment_options:
+for _ in segment_postions:
+    snakey = t.Turtle("square")
+    snakey.color("white")
+    snakey.penup()
+    snakey.goto(_)
+    segments.append(snakey)
+is_game_on = True
     
 
 
