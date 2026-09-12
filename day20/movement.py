c74ff9a9195ca41500be14662aab9e38ebdb4a93
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-12 21:11:34 +0300

    using class and stuff

--- day20/movement.py
+++ day20/movement.py
@@ -1,4 +1,5 @@
 import turtle as t
+import time
 
 
 screen = t.Screen()
@@ -8,20 +9,26 @@ screen.bgcolor("black")
 screen.tracer(0)
 segment_postions =[(0,0),(-20,0),(-40,0)]
 segments = []
-for _ in segment_postions:
-    snakey = t.Turtle("square")
+for postions in segment_postions:
+    snakey = t.Turtle(shape = "square")
     snakey.color("white")
     snakey.penup()
-    snakey.goto(_)
+    snakey.goto(postions)
     segments.append(snakey)
 is_game_on = True
 while is_game_on:
     for seg_num in range(len(segments)-1,0,-1):
-        new_x = segments(seg_num-1).xcor()
-        new_y = segments(seg_num-1).ycor()
-        seg_num.goto(new_x,new_y)
+        new_x = int(segments[seg_num-1].xcor())
+        new_y = int(segments[seg_num-1].ycor())
+        print(new_x,new_y)
+        segments[seg_num].goto(new_x,new_y)
 
+    time.sleep(0.3)
     snakey.forward(10)
+    snakey.right(90)
+    snakey.forward(10)
+    
+    screen.update()
     
 
 
