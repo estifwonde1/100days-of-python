7b86791991a8f390601892ab0b5bf8205d03ee38
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-13 10:15:36 +0300

    using oop

--- day20/movement.py
+++ day20/movement.py
@@ -1,7 +1,9 @@
 import turtle as t
 import time
 
-
+class Snake(self):
+    def __init__(self):
+        self.snake = t.Turtle("square")
 screen = t.Screen()
 screen.setup(width = 600,height = 600)
 screen.title("test subject")
@@ -17,18 +19,19 @@ for postions in segment_postions:
     segments.append(snakey)
 is_game_on = True
 while is_game_on:
+    screen.update()
+    time.sleep(0.1)
     for seg_num in range(len(segments)-1,0,-1):
         new_x = int(segments[seg_num-1].xcor())
         new_y = int(segments[seg_num-1].ycor())
         print(new_x,new_y)
         segments[seg_num].goto(new_x,new_y)
+    segments[0].forward(10)
+    segments[0].right(90)
 
-    time.sleep(0.3)
-    snakey.forward(10)
-    snakey.right(90)
-    snakey.forward(10)
-    
-    screen.update()
+
+
+   
     
 
 
