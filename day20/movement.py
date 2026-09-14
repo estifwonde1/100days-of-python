c8eea3b6393614c29bad9fa05459bd0b64bd7e95
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-14 13:54:57 +0300

    added the error exception

--- day20/movement.py
+++ day20/movement.py
@@ -4,11 +4,15 @@ import time
 
 SEGMENT_POSITIONS =[(0,0),(-20,0),(-40,0)]
 MOVE_DISTANCE = 20
-MOVE_RIGHT = 90
+UP= 90
+DOWN = 270
+RIGHT = 0
+LEFT =180
 class Snake:
     def __init__(self):
         self.segments =[]
         self.create_snake()
+        self.head = self.segments[0]
     def create_snake(self):
         for postions in SEGMENT_POSITIONS:
             snakey = t.Turtle(shape="square")
@@ -21,21 +25,20 @@ class Snake:
             new_x = (self.segments[seg_num-1].xcor())
             new_y = (self.segments[seg_num-1].ycor())
             self.segments[seg_num].goto(new_x,new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
     def move_up(self):
-        # new_heading =self.segments[0].heading() + 90
-        self.segments[0].setheading(90)
-
+        if self.head.heading() != DOWN:
+     
+           self.head.setheading(UP)
     def move_down(self):
-        # new_heading = self.segments[0].heading() + 180
-        self.segments[0].setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(270)
     def move_right(self):
-        # new_heading = self.segments[0].heading() +  0
-        self.segments[0].setheading(0)
-
+        if self.head.heading() != LEFT:
+            self.head.setheading(0)
     def move_left(self):
-        # new_heading = self.segments[0].heading() + 270
-        self.segments[0].setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(180)
 
 
 
