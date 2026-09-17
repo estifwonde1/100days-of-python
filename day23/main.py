e41d23e6fe1ead1484c79dcf1bc9a7b497494c6d
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-17 12:13:14 +0300

    fixed the block and stuff

--- day23/main.py
+++ day23/main.py
@@ -1,18 +1,28 @@
 from turtle import Screen
 from chicken import Chicken
+from obstacle import Obstacle
+import random
+import time
 
+colors = ["hotpink", "darkorange", "gold", "forestgreen", "skyblue", 
+          "turquoise", "royalblue", "purple", "crimson", "darkorchid"]
 screen = Screen()
 screen.setup(width = 600 , height = 600)
 screen.title("Turtle Race")
 screen.tracer(0)
 
 chick = Chicken((0,-280))
+rand_num = random.randrange(-300,301,20)
+color = random.choice(colors)
+obstacle = Obstacle((300,rand_num),color)
 
 screen.listen()
 screen.onkey(key = "Up", fun = chick.move_up)
 game_is_on = True
 while game_is_on:
     screen.update()
+    time.sleep(0.1)
+    obstacle.move()
 
 
 
---
+++ day23/obstacle.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+
+class Obstacle(Turtle):
+    def __init__(self,positions,color):
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.shapesize(1,2)
+        self.goto(positions)
+        self.color(color)
+        
+    def move(self):
+        self.backward(20)
