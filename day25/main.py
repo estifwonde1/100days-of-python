70661dff196abf092355183709a92e356150e3ea
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-20 16:06:47 +0300

    used list comprehension and refactored it

--- day25/main.py
+++ day25/main.py
@@ -10,17 +10,29 @@ screen.register_shape("blank_states_img.gif")
 
 maps=t.Turtle()
 maps.shape("blank_states_img.gif")
-state_xcor =data["x"].to_list()
-state_ycor = data["y"].to_list()
+
 state_name = data["state"].to_list()
+guessed_names =[]
 game_is_on = True
 n = 0
-while game_is_on:
+while len(guessed_names) <50:
+
   
     answer_state = screen.textinput(title=f"{n}/50 Correct",prompt="what is another state")
     print(n)
+    if answer_state == "Exit":
+        missed_states=[state for state in state_name if state not in guessed_names]
+        new_data = pd.DataFrame(missed_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
+    # missed_states=[state for state in state_name if state not in guessed_names]
+
+
+
 
     if answer_state in state_name:
+        guessed_names.append(answer_state)
         answer = data[data.state == answer_state] 
         x_cor=answer.x.to_list()
         y_cor=answer.y.to_list()  
