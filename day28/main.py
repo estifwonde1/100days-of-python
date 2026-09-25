e036a218f111ad9a83a20969a629f09e894d51a6
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-25 18:51:48 +0300

    the UI is done now the functionality

--- day28/main.py
+++ day28/main.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from PIL import ImageTk,Image
 import time
 
 PINK = "#FFC0CB"
@@ -10,25 +9,44 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK = 20
+testers=["00:01","00:02","00:03","00:04","00:05","00:06"]
 
 
 window = Tk()
 window.title("Pomodro techinique")
-tomato = Image.open("Tomato.jpg")
-tk_image = ImageTk.PhotoImage(tomato)
+tomato = PhotoImage(file ="tomato.png")
 
-image_label =Label(image = tk_image)
-def count_down(down):
- 
-    for n in range(0,down):
-        time.sleep(1)
-        print(n)
+canvas = Canvas(width=525,height=550 )
+canvas.create_image(260,270,image=tomato)
+timer_change=canvas.create_text(260,290,text="00:00",fill="white" ,font=(FONT_NAME,35,"bold"))
+canvas.pack()
+
+def change_text(n):
+    # timer_change=canvas.create_text(260,290,text="00:00",fill="white" ,font=(FONT_NAME,35,"bold"))
+    canvas.itemconfig(timer_change,text=n)
     
-count =Label(text=30)
-count.grid(column = 1, row = 1)
+
+def count_down():
+ 
+    for n in testers:       
+        print(n)
+        change_text(n)
+        time.sleep(1)
+        canvas.after(1000,count_down)
 
 
-count_down(10)
+
+        
+        
+    
+
+
+
+
+reset = Button(text= "Reset" )
+start=Button(text="start",command=count_down)
+canvas.create_window(200,500,window=start)
+canvas.create_window(350,500,window=reset)
 
     
     
