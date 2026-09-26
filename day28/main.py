1dd2692d34686d351e0ddeb45e5ce6659bfc6306
estifwonde1 <estifanoswondwossen396@gmail.com>
2026-09-26 11:08:07 +0300

    the count_down is partially functional

--- day28/main.py
+++ day28/main.py
@@ -9,42 +9,42 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK = 20
-testers=["00:01","00:02","00:03","00:04","00:05","00:06"]
+testers=["00:00","00:01","00:02","00:03","00:04","00:05","00:06"]
 
 
 window = Tk()
 window.title("Pomodro techinique")
 tomato = PhotoImage(file ="tomato.png")
-
 canvas = Canvas(width=525,height=550 )
 canvas.create_image(260,270,image=tomato)
-timer_change=canvas.create_text(260,290,text="00:00",fill="white" ,font=(FONT_NAME,35,"bold"))
+timer_change=canvas.create_text(260,290,text= 0 ,fill="white" ,font=(FONT_NAME,35,"bold"))
 canvas.pack()
 
-def change_text(n):
-    # timer_change=canvas.create_text(260,290,text="00:00",fill="white" ,font=(FONT_NAME,35,"bold"))
-    canvas.itemconfig(timer_change,text=n)
+# def say_something(a,b,c,):
     
 
-def count_down():
- 
-    for n in testers:       
-        print(n)
-        change_text(n)
-        time.sleep(1)
-        canvas.after(1000,count_down)
+#     print(a)
+#     print(b)
+#     print(c)
+def counter(count):
+    print(count)
+    canvas.itemconfig(timer_change, text = count)
+    window.after(1000,counter,count - 1)
+
+    
+
+
 
 
 
         
-        
-    
+counter(5)
 
 
 
 
 reset = Button(text= "Reset" )
-start=Button(text="start",command=count_down)
+start=Button(text="start")
 canvas.create_window(200,500,window=start)
 canvas.create_window(350,500,window=reset)
 
